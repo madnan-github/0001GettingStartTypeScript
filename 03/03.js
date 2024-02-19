ed4ab898ad57fe083ec9596e517168751053d13d
Muhammad Adnan <dononlineboss@gmail.com>
2024-02-19 21:30:26 +0500

    done 03

--- 03/03.js
+++ 03/03.js
@@ -1,9 +1,14 @@
 "use strict";
 //Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-let u_Name = "mUHammAD adNAN";
-let u_Case = u_Name.toUpperCase();
-console.log(u_Case);
-let l_Case = u_Name.toLowerCase();
+let p_Name = "mUHammAD adNAN";
+let p_Case = p_Name.toUpperCase();
+console.log(p_Case);
+let l_Case = p_Name.toLowerCase();
 console.log(l_Case);
-let t_Case = u_Name.split(" ");
+let words = p_Name.split(" ");
+let t_Case = "";
+for (let i = 0; i < words.length; i++) {
+    t_Case += words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase() + " ";
+}
+;
 console.log(t_Case);
--- 03/03.ts
+++ 03/03.ts
@@ -1,19 +1,19 @@
 //Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
 
-let u_Name : string = "mUHammAD adNAN";
+let p_Name : string = "mUHammAD adNAN";
 
-let u_Case : string = u_Name.toUpperCase();
-console.log(u_Case);
+let p_Case : string = p_Name.toUpperCase();
+console.log(p_Case);
 
-let l_Case : string = u_Name.toLowerCase();
+let l_Case : string = p_Name.toLowerCase();
 console.log(l_Case);
 
-/*
-let words : string[] = u_Name.split(" ");
-let t_Case : string = ""
-for (let i = 0; i < words.length; i++) {
-    t_Case = words[i].toUpperCase + words.splice(0)
-        
-    
-}*/
+
+let words : string[] = p_Name.split(" ");
+let t_Case: string = "";
+for (let i = 0 ; i < words.length; i++) {
+    t_Case += words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase() + " "
+};
+
+console.log(t_Case)
 
