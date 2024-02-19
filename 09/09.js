19193d14fca38d1d8280621694502cdef7184af2
Muhammad Adnan <dononlineboss@gmail.com>
2024-02-19 23:31:33 +0500

    done 09

---
+++ 09/09.js
@@ -0,0 +1,5 @@
+"use strict";
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let fav_Num = 786;
+let msg = `My Favorite Number is: ${fav_Num}`;
+console.log(msg);
---
+++ 09/09.ts
@@ -0,0 +1,9 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let fav_Num : number = 786;
+
+let msg : string = `My Favorite Number is: ${fav_Num}`;
+console.log(msg);
+
+
+
