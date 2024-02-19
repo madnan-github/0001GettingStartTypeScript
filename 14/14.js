22db86e64327cceb16562923e208f21b564f72bd
Muhammad Adnan <dononlineboss@gmail.com>
2024-02-20 00:57:06 +0500

    done 14

---
+++ 14/14.js
@@ -0,0 +1,6 @@
+"use strict";
+//Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+let guest_Name = ["Ammad", "Naeem", "Salam", "Qader"];
+guest_Name.forEach((guest_Name) => {
+    console.log(`Dear ${guest_Name}, you are invited to dnner please  join us.`);
+});
