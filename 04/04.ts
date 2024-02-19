cf65ac0bac35eddea21619035dba5936a5368f27
Muhammad Adnan <dononlineboss@gmail.com>
2024-02-19 21:36:16 +0500

    done 04

---
+++ 04/04.ts
@@ -0,0 +1,3 @@
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+console.log('Muhammad Ali Jinnah Said, "Think 100 times before you take a decision, But once that decision is taken, stand by it as one man. ..."');
