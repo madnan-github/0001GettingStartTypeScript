a547e9c8b3a7d4be3997c4413f9348f30d7c9c4e
Muhammad Adnan <dononlineboss@gmail.com>
2024-02-19 22:32:38 +0500

    done 08

---
+++ 08/08.ts
@@ -0,0 +1,16 @@
+//You should create four lines that look like this:
+//--------------------------------
+//console.log(5 + 3)
+//--------------------------------
+
+let line_Char = "-"
+
+console.log(line_Char.repeat(50));
+console.log(5 + 3);
+console.log(line_Char.repeat(50));
+console.log(16 - 8);
+console.log(line_Char.repeat(50));
+console.log(4 * 2);
+console.log(line_Char.repeat(50));
+console.log(16 / 2);
+console.log(line_Char.repeat(50));
